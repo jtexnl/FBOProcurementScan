e5dee67bef116c15de02317962770e1bb5145e47
jtexnl <jtexnl@gmail.com>
2016-08-31 14:56:46 -0400

    reorganizing file structure

--- scripts/run/FetchNewData.py
+++
@@ -1,3 +0,0 @@
-import datetime
-
-listURL = 'https://api.data.gov/gsa/fbopen/v0/opps?q=posted_dt:2016-08-23T00:00:00Z&type:attachment&data_source=FBO&limit=500&api_key=bmPVCo4hkTV2SWMWjXEvx7XYl6gYc77BpGlhseXq'
