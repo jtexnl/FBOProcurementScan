d79a6b2068e207eb00aa3c92c075cba2f5de0e8a
jtexnl <jtexnl@gmail.com>
2016-11-28 15:47:47 -0500

    cleaning up code after test run

--- scripts/run.py
+++ scripts/run.py
@@ -3,7 +3,6 @@ import os
 from spacy.en import English
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
-import dataHandling
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.externals import joblib
 import numpy as np
@@ -19,21 +18,22 @@ solicitations = []
 for i in range(0, len(rawData.raw)):
     solicitations.append(classes.newSolicitation(rawData.raw, i))
 
+### This section may not be necessary with the addition of predictionGenerator() object to classes. Check in.
 print('loading predictors')
-ridge = joblib.load('ridge.pkl')
-nearestCentroid = joblib.load('nearestCentroid.pkl')
-L2SVC = joblib.load('L2SVC.pkl')
-BNB = joblib.load('BNB.pkl')
-knn = joblib.load('knn.pkl')
-perceptron = joblib.load('perceptron.pkl')
-pipeline = joblib.load('pipeline.pkl')
-L2SGD = joblib.load('L2SGD.pkl')
-elasticNet = joblib.load('elasticNet.pkl')
-L1SVC = joblib.load('L1SVC.pkl')
-MNB = joblib.load('MNB.pkl')
-L1SGD = joblib.load('L1SGD.pkl')
-passiveAggressive = joblib.load('passiveAggressive.pkl')
-randomForest = joblib.load('randomForest.pkl')
+ridge = joblib.load('binaries/ridge.pkl')
+nearestCentroid = joblib.load('binaries/nearestCentroid.pkl')
+L2SVC = joblib.load('binaries/L2SVC.pkl')
+BNB = joblib.load('binaries/BNB.pkl')
+knn = joblib.load('binaries/knn.pkl')
+perceptron = joblib.load('binaries/perceptron.pkl')
+pipeline = joblib.load('binaries/pipeline.pkl')
+L2SGD = joblib.load('binaries/L2SGD.pkl')
+elasticNet = joblib.load('binaries/elasticNet.pkl')
+L1SVC = joblib.load('binaries/L1SVC.pkl')
+MNB = joblib.load('binaries/MNB.pkl')
+L1SGD = joblib.load('binaries/L1SGD.pkl')
+passiveAggressive = joblib.load('binaries/passiveAggressive.pkl')
+randomForest = joblib.load('binaries/randomForest.pkl')
 
 predictors = {'ridge':ridge, 'nearestCentroid':nearestCentroid, 'L2SVC':L2SVC, 'BNB':BNB, 'knn':knn, 'perceptron':perceptron, 'pipeline':pipeline, 'L2SGD':L2SGD, 'elasticNet':elasticNet, 'L1SVC':L1SVC, 'MNB':MNB, 'L1SGD':L1SGD, 'passiveAggressive':passiveAggressive, 'randomForest':randomForest}
 
@@ -50,7 +50,7 @@ for item in predictionList:
     predictionText.append(item['text'])
 
 print('vectorizing text')
-vec = joblib.load('vectorizer.pkl')
+vec = joblib.load('binaries/vectorizer.pkl')
 
 predictionVector = vec.transform(predictionText)
 
@@ -77,8 +77,4 @@ for i in predictionList:
 
 formatted = classes.formattedPredictionOutput(raw, solicitations)
 
-def writeJson(inputData, fileName):
-    with open(fileName, 'w+') as outfile:
-        json.dump(inputData, outfile, indent = 4)
-
-writeJson(formatted.finalOutput, 'predictions_' + str(rawData.date) + '.json')
+dataHandling.writeJson(formatted.finalOutput, 'predictions/predictions_' + str(rawData.date) + '.json')
